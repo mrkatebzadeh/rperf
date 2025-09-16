fbed678640831b317ed2daf2732d60eaa2e284db
Siavash Katebzadeh <mr.katebzadeh@gmail.com>
2025-09-16 10:07:46 +0000

    Cleaned

--- src/args.rs
+++ src/args.rs
@@ -25,21 +25,9 @@ use std::fmt::{self, Display};
 #[derive(Parser)]
 #[command(version, about, long_about = None, term_width = 80)]
 pub struct Args {
-    #[arg(short = 'd', long, value_name = "device", help = "RDMA device name")]
-    #[clap(default_value = "mlx5_0")]
-    pub device: String,
-
-    #[arg(short = 'p', long, help = "RDMA Port")]
-    #[clap(default_value_t = 0)]
-    pub ib_port: usize,
-
     #[arg(short = 'c', long, value_name = "FILE", help = "Configuration file")]
     #[clap(default_value = "config.toml")]
     pub config: String,
-
-    #[arg(short, long, help = "Number of total operations")]
-    #[clap(default_value_t = 100000)]
-    pub operations: u32,
 
     #[arg(short, long, action = clap::ArgAction::Count)]
     #[arg(help = "Increment verbosity level (repeat for more detail, e.g., -vvv)")]
@@ -51,12 +39,9 @@ impl Display for Args {
         write!(
             f,
             "Args:\n\
-            Device:     {}\n\
-            IB Port:    {}\n\
             Config:     {}\n\
-            Operations: {}\n\
             Verbose:    {}",
-            self.device, self.ib_port, self.config, self.operations, self.verbose
+            self.config, self.verbose
         )
     }
 }

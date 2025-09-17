70716d93c9b808fd923fb30ee448c4f18c6376fc
Siavash Katebzadeh <mr.katebzadeh@gmail.com>
2025-09-17 16:50:01 +0000

    Fixed sampling

--- src/probe.rs
+++ src/probe.rs
@@ -19,7 +19,17 @@
 * along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-use crate::{adaptor::Adaptor, message::Message, Config};
+use std::{
+    sync::{
+        atomic::{AtomicBool, Ordering},
+        Arc,
+    },
+    thread,
+};
+
+use spdlog::info;
+
+use crate::{adaptor::Adaptor, config, message::Message, server::Server, Config};
 
 pub(crate) struct Probe {
     config: Config,
@@ -30,15 +40,52 @@ impl Probe {
     }
 
     pub(crate) fn start(&mut self) -> anyhow::Result<()> {
-        let mut adaptor = Adaptor::connect(self.config.clone())?;
+        let mut config = self.config.clone();
+        let running = Arc::new(AtomicBool::new(true));
+        let r = running.clone();
+        let srv_handler = thread::spawn(move || {
+            info!("Starting probe loopback server");
+            config.is_agent = true;
+            config.connection.server_addr = "0.0.0.0".to_string();
+            config.connection.server_port -= 1;
+            let loopback_srv = Server::bind(config).unwrap();
+            info!("Probe loopback server connected");
+            let loopback_adaptor = loopback_srv.accept().unwrap();
+            while r.load(Ordering::Relaxed) {
+                let _ = loopback_adaptor.read();
+            }
+        });
+
+        info!("Connecting to agent");
+        let mut wire_adaptor = Adaptor::connect(self.config.clone())?;
+        info!("Connecting to loopback server");
+        let mut loopback_adaptor = {
+            let mut config = self.config.clone();
+            config.connection.server_addr = "0.0.0.0".to_string();
+            Adaptor::connect(self.config.clone())?
+        };
         let total_iters = self.config.test.iterations;
         let warmup_iters = (total_iters as f64 * 0.1).ceil() as usize;
+        let msg_size = self.config.test.msg_size;
+        info!("Warming up");
         for id in 0..warmup_iters {
-            adaptor.write(&[Message::new(self.config.test.msg_size, id as u64)], true);
+            let id = id as u64;
+            let _ = loopback_adaptor.get_rtt(&[Message::new(msg_size, id)]);
+            let _ = wire_adaptor.get_rtt(&[Message::new(msg_size, id)]);
         }
+        info!("Test started");
         for id in 0..total_iters {
-            adaptor.write(&[Message::new(self.config.test.msg_size, id as u64)], false);
+            let id = id as u64;
+            let loop_rtt = loopback_adaptor.get_rtt(&[Message::new(msg_size, id)]);
+            let wire_rtt = wire_adaptor.get_rtt(&[Message::new(msg_size, id)]);
+
+            wire_adaptor.tx_collector.insert((wire_rtt, loop_rtt));
         }
+
+        wire_adaptor.tx_collector.dump_csv();
+        info!("Test finished");
+        running.store(false, Ordering::Relaxed);
+        srv_handler.join().unwrap();
         Ok(())
     }
 }
